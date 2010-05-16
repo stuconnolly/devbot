14205821790641f7db3616858aaaa4e74572ea08
Stuart Connolly <stuart02@gmail.com>
2010-05-16 06:48:46 +0000

    Add main script.

---
+++ bin/gcbot.pl
@@ -0,0 +1,32 @@
+#! /usr/bin/perl
+
+#
+#  $Id$
+#  
+#  gcbot
+#  http://dev.stuconnolly.com/svn/gcbot/
+#
+#  Copyright (c) 2010 Stuart Connolly. All rights reserved.
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+package GCBot;
+
+use strict;
+use warnings;
+
+use lib '../lib';
+
+exit 0
