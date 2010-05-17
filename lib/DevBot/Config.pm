21cea5b33a57ba05fdc413e0ffe86826b63febc0
Stuart Connolly <stuart02@gmail.com>
2010-05-17 15:03:00 +0000

    Complete renaming.

--- lib/DevBot/Config.pm
+++ lib/DevBot/Config.pm
@@ -20,7 +20,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-package GCBot::Config;
+package DevBot::Config;
 
 use strict;
 use warnings;
--- lib/GCBot/Bot.pm
+++ lib/GCBot/Bot.pm
@@ -22,7 +22,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-package GCBot::Bot;
+package DevBot::Bot;
 
 use strict;
 use warnings;
