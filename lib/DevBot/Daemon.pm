6453faa59d137c399adec8f2f19d0af7ec2a05a7
Stuart Connolly <stuart02@gmail.com>
2012-11-19 22:12:30 +0000

    Reduce the size of the deamon's run method.

--- lib/DevBot/Daemon.pm
+++ lib/DevBot/Daemon.pm
@@ -1,11 +1,11 @@
 #
 #  $Id$
-#  
+#
 #  devbot
 #  http://dev.stuconnolly.com/svn/devbot/
 #
 #  Copyright (c) 2010 Stuart Connolly. All rights reserved.
-# 
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -40,9 +40,9 @@ our $VERSION = '1.00';
 sub new
 {
 	my ($this, $host, $port, $gc_key, $m_key, $message) = @_;
-	
+
 	my $class = ref($this) || $this;
-				
+
 	my $self = {
 		_host    => $host,
 		_port    => $port,
@@ -50,19 +50,19 @@ sub new
 		_m_key   => $m_key,
 		_message => $message
 	};
-	
+
 	bless($self, $class);
-	
+
 	return $self;
 }
 
 #
-# Starts listening for commit notifications by initialising and starting the HTTP daemon.
+# Starts listening for commit notifications and messages by initialising and starting the HTTP daemon.
 #
 sub run
 {
 	my $self = shift;
-	
+
 	my $daemon = HTTP::Daemon->new(
 					ReuseAddr => 1,
 					Listen    => 20,
@@ -70,53 +70,70 @@ sub run
 					LocalAddr => $self->{_host},
 					LocalPort => $self->{_port}
 					) || carp 'Failed to create HTTP daemon';
-		
+
 	while (my $connection = $daemon->accept)
-	{			
-		while (my $request = $connection->get_request) 
+	{
+		while (my $request = $connection->get_request)
 		{
 			my $method = $request->method;
 			my $path   = $request->uri->path;
 
 			if (($method eq 'POST') && ($path eq '/commit')) {
-				
-				$connection->send_response(HTTP::Response->new(RC_OK));
-				
-				# Announce the results to the channel
-				foreach (DevBot::Commit->new($request, $self->{_gc_key})->parse) 
-				{
-					DevBot::Bot::say($_) if (length);
-				}
+				_handle_commit($connection, $request);
 			}
 			elsif (($method eq 'POST') && ($path eq '/message') && $self->{_message}) {
-				
-				if (DevBot::Auth::authenticate_message_request($request, $self->{_m_key})) {
-					
-					$connection->send_response(HTTP::Response->new(RC_OK));
-
-					my $message = $request->content;
-
-					# Strip leading and trailing whitespace
-					$message =~ s/^\s+//;
-					$message =~ s/\s+$//;
-					
-					$message =~ s/r([0-9]+)/DevBot::Project::create_revision_url($1)/gie;
-
-					# Simply print the message to STDOUT and the bot will announce it to the channel
-					DevBot::Bot::say($message) if (length($message));
-				}
-				else {
-					$connection->send_response(HTTP::Response->new(RC_UNAUTHORIZED));
-				}
+				_handle_message($connection, $request);
 			}
 			else {
 				$connection->send_error(RC_FORBIDDEN);
 			}
-		}	
+		}
 
 		$connection->close;
 		undef($connection);
-	}	
+	}
+}
+
+#
+# Handle a commit via the supplied request and connection.
+#
+sub _handle_commit
+{
+	my ($self, $connection, $reuqest) = $_;
+
+	$connection->send_response(HTTP::Response->new(RC_OK));
+
+	# Announce the results to the channel
+	foreach (DevBot::Commit->new($request, $self->{_gc_key})->parse)
+	{
+		DevBot::Bot::say($_) if (length);
+	}
+}
+
+#
+# Handle a message via the supplied request and connection.
+#
+sub _handle_message
+{
+	my ($self, $onnection, $request) = $_;
+
+	if (DevBot::Auth::authenticate_message_request($request, $self->{_m_key})) {
+		$connection->send_response(HTTP::Response->new(RC_OK));
+
+		my $message = $request->content;
+
+		# Strip leading and trailing whitespace
+		$message =~ s/^\s+//;
+		$message =~ s/\s+$//;
+
+		$message =~ s/r([0-9]+)/DevBot::Project::create_revision_url($1)/gie;
+
+		# Simply print the message to STDOUT and the bot will announce it to the channel
+		DevBot::Bot::say($message) if (length($message));
+	}
+	else {
+		$connection->send_response(HTTP::Response->new(RC_UNAUTHORIZED));
+	}
 }
 
 1;
