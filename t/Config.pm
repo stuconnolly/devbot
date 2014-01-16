7a98dd5e2128ed9bbddc8b8e6e07086fa16a2133
Stuart Connolly <stuart02@gmail.com>
2014-01-16 00:01:07 +0000

    Add tests.

---
+++ t/Config.pm
@@ -0,0 +1,41 @@
+#  
+#  devbot
+#  https://github.com/stuconnolly/devbot
+#
+#  Copyright (c) 2010 Stuart Connolly. All rights reserved.
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+package t::Config;
+
+use strict;
+use warnings;
+
+use Cwd;
+use Test::More tests => 2;
+
+use base 'Test::Class';
+
+BEGIN 
+{ 
+	use_ok('DevBot::Config');
+}
+
+sub test_config_loads : Test
+{
+	ok(DevBot::Config::get('test.conf'), 'Config loads');
+}
+
+1;
